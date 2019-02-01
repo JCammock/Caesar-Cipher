d37cdd8bc2a49c877ca1af447c945c00ec7c5dcf
ChronoPixel <joshua.cammock@hotmail.com>
2019-02-01 22:47:02 +0000

    Complete Caeser Cipher using 3 shift

---
+++ Cipher/Encryption.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher
+{
+    class Encryption
+    {
+        private string unencrypted;
+
+        public void SetUnencrypted(string unencrypted)
+        {
+            this.unencrypted = unencrypted;
+        }
+
+        public string Encrypt(int shift)
+        {
+            char[] buffer = unencrypted.ToCharArray();
+            for(int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                letter = (char)(letter + shift);
+                if (letter > 'z')
+                {
+                    letter = (char)(letter - 26);
+                }
+                else if (letter < 'a')
+                {
+                    letter = (char)(letter + 26);
+                }
+                buffer[i] = letter;
+            }
+            return new string(buffer);
+
+        }
+    }
+}
--- Cipher/Program.cs
+++ Cipher/Program.cs
@@ -10,6 +10,14 @@ namespace Cipher
     {
         static void Main(string[] args)
         {
+            Encryption e = new Encryption();
+
+            Console.WriteLine("Please enter the string to encrypt");
+            e.SetUnencrypted(Console.ReadLine());
+            string encrypted = e.Encrypt(3);
+            Console.WriteLine("Your encrypted string is: " + encrypted);
+            Console.Read();
+
         }
     }
 }
